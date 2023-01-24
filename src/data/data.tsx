52aec7ef5d9aed1e886304f98ff6775037ebf744
Vrezh Fedora <vrezhraiden@gmail.com>
2023-01-24 20:20:51 +0100

    sterio comment

--- src/data/data.tsx
+++ src/data/data.tsx
@@ -271,6 +271,11 @@ export const testimonial: TestimonialSection = {
       text: 'Great communication, quick feedback and support for each project was a good surprise.',
       image: 'http://static1.squarespace.com/static/5e77a8d31f1ca16b984e7d8c/t/5f2315f95dcf3778e4e4eaa6/1598456607711/%3F%3F%3F%3F%3F%3F_%3F%3F%3F%3F%3F%3F%3F%3F+%D1%8F+%D0%BE%D0%B1%D0%BB%D0%B0%D1%81%D1%82%D1%8C+1.jpg?format=1500w',
     },
+    {
+      name: 'Sterio',
+      text: 'Vrezh has great hard & soft skills. It was a pleasure to develop complex applications with him!',
+      image: 'https://avatars.githubusercontent.com/u/55721185?v=4',
+    },
   ],
 };
 
